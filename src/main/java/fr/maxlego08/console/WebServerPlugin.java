9d80c0d5dac840e5cab730407375f7601f855753
Maxence <simon.maxence@hotmail.com>
2026-02-10 17:41:38 +0100

    :tada: First commit

---
+++ src/main/java/fr/maxlego08/console/WebServerPlugin.java
@@ -0,0 +1,20 @@
+package fr.maxlego08.console;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class WebServerPlugin extends JavaPlugin {
+
+    private Thread wsThread;
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
